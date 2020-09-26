70d41e5f42a831d8ad76e50f61c62f95c8993207
Antonio Alvarez <criticalgeek1@gmail.com>
2020-09-25 22:10:56 -0500

    Add text component

---
+++ src/components/Text.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import styles from '../scss/components/text.scss';
+
+export default (props) => {
+  return <Text style={styles.small}>{props.children}</Text>;
+};
