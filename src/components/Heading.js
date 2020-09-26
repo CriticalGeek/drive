c06490f7dd49dd3162be81861c27047813aaaa29
Antonio Alvarez <criticalgeek1@gmail.com>
2020-09-25 22:10:43 -0500

    Add heading component

---
+++ src/components/Heading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import styles from '../scss/components/heading.scss';
+
+export default (props) => {
+  return <Text style={styles.heading}>{props.children}</Text>;
+};
