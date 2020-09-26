1b02d8bbb406c8d2af37c2a3185855f2935c4014
Antonio Alvarez <criticalgeek1@gmail.com>
2020-09-26 15:50:43 -0500

    Add signup screen

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,9 +1,41 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View} from 'react-native';
 
-export default ({history}) => (
-  <View>
-    <Text>Signup page</Text>
-    <Button title="to login screen" onPress={() => history.push('/')} />
-  </View>
-);
+import layout from '../scss/layout/login.scss';
+import styles from '../scss/screens/signup.scss';
+
+import Appbar from '../components/Appbar';
+import Heading from '../components/Heading';
+import Textfield from '../components/Textfield';
+import Button from '../components/Button';
+
+class Signup extends React.Component {
+  render() {
+    return (
+      <View>
+        <Appbar hasBackward />
+
+        <View style={layout.login}>
+          <Heading style={styles.heading}>
+            Sube a bordo y comienza a disfrutar de DRIVE.
+          </Heading>
+
+          <Textfield style={styles.field} placeholder="Nombre completo" />
+          <Textfield style={styles.field} placeholder="Correo electrónico" />
+          <Textfield
+            style={styles.field}
+            placeholder="Contraseña"
+            secureEntry
+          />
+          <Textfield placeholder="Confirma tu contraseña" secureEntry />
+
+          <Button style={styles.button} block>
+            Registrarme
+          </Button>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default Signup;
