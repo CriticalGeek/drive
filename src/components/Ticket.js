ad46beb0f5cd42edc93704fe2f2eb9adbace8d4a
Antonio Alvarez <criticalgeek1@gmail.com>
2020-09-26 23:25:13 -0500

    Add ticket component

---
+++ src/components/Ticket.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import styles from '../scss/components/ticket.scss';
+
+import Heading from '../components/Heading';
+import Text from '../components/Text';
+
+export default ({item, style}) => {
+  return (
+    <View style={{...styles.ticket, ...style}}>
+      <View style={styles.date}>
+        <Heading>01</Heading>
+        <Text>Ene</Text>
+      </View>
+      <View>
+        <Text bold>Servicio de estacionamiento</Text>
+        <Text small>No. de ticket: {item.number}</Text>
+        <Text style={styles.amount}>Total: MXN {item.amount}</Text>
+      </View>
+    </View>
+  );
+};
