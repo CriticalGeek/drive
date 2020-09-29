5252c27409e0dbae403c38842e0c1b66a668d8d0
Antonio Alvarez <criticalgeek1@gmail.com>
2020-09-28 22:36:05 -0500

    Fix static date in ticket component

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import { View } from 'react-native';
+import dayjs from 'dayjs';
 
 import styles from '~/scss/components/ticket.scss';
 
 import Heading from './Heading';
 import Text from './Text';
 
+const customParseFormat = require('dayjs/plugin/customParseFormat');
+dayjs.extend(customParseFormat);
+
+const months = [
+  'Ene',
+  'feb',
+  'Mar',
+  'Abr',
+  'May',
+  'Jun',
+  'Jul',
+  'Ago',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dic',
+];
+
 export default ({ item, style }) => {
+  const date = dayjs(item.date, 'YYYY-MM-DD HH:mm:ss');
+  const month = months[dayjs(date).month()];
+  const day = dayjs(date).date();
+
   return (
     <View style={{ ...styles.ticket, ...style }}>
       <View style={styles.date}>
-        <Heading>01</Heading>
-        <Text>Ene</Text>
+        <Heading>{day}</Heading>
+        <Text>{month}</Text>
       </View>
       <View>
         <Text bold>Servicio de estacionamiento</Text>
